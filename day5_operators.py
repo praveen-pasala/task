75a2488a9dd90e6d4e705da8d27128315d5b3944
Pasala Nagapraveen <praveen.7780687851@gmail.com>
2026-05-21 19:08:42 +0530

    Initial commit

---
+++ day5_operators.py
@@ -0,0 +1,87 @@
+# a=10
+# b=20
+# print(a+b)
+
+
+# a="naga"
+# b="praveen"
+# print(a+b)
+
+# a=10
+# b="praveen"
+# print(str(a)+b)
+
+# a="naga"
+# b="praveen"
+# print(a+b)
+
+
+# age1=int(input("enter your age: "))
+# age2=int(input("enter your fathers age: "))
+# print("sum of ages: ",age1+age2)
+
+
+# age1=input("enter your age: ")
+# age2=input("enter your fathers age: ")
+# print("sum of ages: ",int(age1)+int(age2))
+
+
+# a=10
+# b=3
+# print("sum: ",a+b)
+# print("difference: ",a-b)
+# print("product: ",a*b)
+# print("quotient: ",a/b)
+# print("floor division: ",a//b)
+# print("modulus: ",a%b)
+# print("exponentiation: ",a**b)
+
+# a=0
+# b=3
+# c=1
+# d=10
+# print("modulus: ",a%b)
+# print("modulus: ",c%b)
+# print("modulus: ",d%a)
+# print("modulus: ",b%a)
+
+
+# print(1%3)
+# print(2%3)
+# print(3%3)
+# print(0%3)
+# print(4%3)
+# print(3/0)
+# print(3//0)
+# print(3**0)
+# print(3%0)
+
+
+# print(10<20)
+# print(10>20)
+# print(10<=20)
+# print(10>=20)
+# print(10==20)
+# print(10!=20)
+# print(10==10)
+# print(10!=10)
+
+
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+print(a and not b)
+print(not a or b)
+print(a or not b)
+print(not a and b)
+print((a and b) or (not a and not b))
+print((a or b) and (not a or not b))
+print((a and not b) or (not a and b))
+print((a or not b) and (not a or b))
+print((a and b) or (a and not b) or (not a and b) or (not a and not b))
+print((a or b) and (a or not b) and (not a or b) and (not a or not b))
+
+
